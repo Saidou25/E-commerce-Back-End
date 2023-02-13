972ea3d33889f5bbd990e0d61e39abddd45b693e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-12 21:22:29 -0500

    following with api's by adding create, and delete to category and tag, all work in Insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async(req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No library card found with that id!' });
+      res.status(404).json({ message: 'No lCategory found with that id!' });
       return;
     }
 
@@ -35,16 +35,38 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async(req, res) => {
     });
 
     if (!tagtData) {
-      res.status(404).json({ message: 'No library card found with that id!' });
+      res.status(404).json({ message: 'No Tag found with that id!' });
       return;
     }
 
@@ -36,16 +36,39 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const tagtData = await Tag.create(req.body);
+    res.status(200).json(tagtData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagtData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagtData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagtData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
 module.exports = router;
